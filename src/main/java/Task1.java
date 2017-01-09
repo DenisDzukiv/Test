f8efff89eb133c20a5c816ad5c2c2a2aadbf1696
DenisDzukiv <zhukivskii.1988@mail.ru>
2017-01-09 19:53:08 +0300

    Second commit

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,8 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
- * Created by Денис on 09.01.2017.
+ * На вход программе задаются два IP адреса, программа перебирает все
+ допустимые адреса в введенном диапазоне и выдает их на экран.
  */
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String ipBegin = reader.readLine();
+            int lastIpBegin = split(ipBegin,3);
+            String ipLast = reader.readLine();
+            int lastIpLast = split(ipLast, 3);
+
+            if (lastIpBegin<lastIpLast) {
+                for (int i = 0; i<=(lastIpLast-lastIpBegin); i++){
+                    lastIpBegin ++;
+                    System.out.println(split(ipBegin,0)+ "." + split(ipBegin,1) + "." + split(ipBegin,2) + "." + lastIpBegin );
+                }
+            }
+            else System.out.println("Поменяйте IP местами");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int split(String ip, int getNumber){
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i<ip.split("\\.",4).length; i++) {
+            list.add(Integer.parseInt(ip.split("\\.",4)[i]));
+        }
+
+        return list.get(getNumber);
     }
 }
